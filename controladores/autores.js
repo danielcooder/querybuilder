a2211b773942b0d8c90318e22ebe14aedcf2ec7b
daniel <danielcooder@gmail.com>
2024-06-20 16:22:44 -0300

    perf: atualizando autor

--- controladores/autores.js
+++ controladores/autores.js
@@ -6,7 +6,7 @@ const listarAutores = async (req, res) => {
 
         return res.status(200).json(autores);
     } catch (erro) {
-        return res.status(400).json(error.message);
+        return res.status(400).json(erro.message);
     }
 }
 
@@ -55,6 +55,31 @@ const cadastrarAutor = async (req, res) => {
 }
 
 const atualizarAutor = async (req, res) => {
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+
+    try {
+        const autor = await conexao.query('select * from autores  where id = $1', [id]);
+
+        if (autor.rowCount === 0) {
+            return res.status(404).json('Autor não encontrado.')
+        }
+
+        if (!nome) {
+            return res.status(400).json("O campo nome é obrigatório.")
+        }
+
+        const query = 'update autores set nome = $1, idade = $2 where id = $3';
+        const autorAtualizado = await conexao.query(query, [nome, idade, id]);
+
+        if (autorAtualizado.rowCount === 0) {
+            return res.status(404).json('Não foi possivel atualizar o autor')
+        }
+
+        return res.status(200).json('Autor foi atualizado com sucesso.');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
 
 }
 
