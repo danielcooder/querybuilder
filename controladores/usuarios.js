56af2c162ab4edea318ffb20d629e4c513cb29c3
shaienne23 <shaienneo@gmail.com>
2024-06-28 15:39:18 -0300

    perf: login verificacao de senha

--- controladores/usuarios.js
+++ controladores/usuarios.js
@@ -1,3 +1,4 @@
+
 const conexao = require("../conexao");
 const SecurePassword = require("secure-password");
 
@@ -42,4 +43,64 @@ const cadastrarUsuario = async (req, res) => {
   }
 };
 
-module.exports = { cadastrarUsuario };
+
+
+const login = async (req, res) => {
+const { email, senha} = req.body;
+
+
+if (!email) {
+    return res.status(400).json("O campo é obrigatorio.");
+}
+
+if (!senha) {
+    return res.status(400).json("O campo senha é obrigatorio.")
+}
+
+try {
+const query = 'select * from usuarios where email = $1';
+const usuarios = await conexao.query(query, [email]);
+
+if (usuarios.rowCount === 0){
+    return res.status(400).json("Email ou senha incorretos.")
+}
+
+const usuario = usuarios.rows[0];
+
+const result = await pwd.verify(Buffer.from(senha), Buffer.from(usuario.senha, "hex"));
+
+switch (result) {
+
+case SecurePassword.INVALID_UNRECOGNIZED_HASH:
+case SecurePassword.INVALID:
+    return res.status(400).json("Email ou senha incorretos.");
+
+case SecurePassword.VALID:
+    break;
+case SecurePassword.VALID_NEEDS_REHASH:
+
+try {
+  
+    const hash = (await pwd.hash(Buffer.from(senha))).toString("hex");
+    const query = 'update usuarios set senha = $1 where email = $2';
+    await conexao.query(query, [hash, email]);
+
+} catch {
+
+}break;
+
+}
+
+return res.json(`Tudo certo! Seja bem vindo, ${usuario.nome}`);
+
+} catch (error) {
+
+    return res.status(400).json(error.message);
+}
+
+
+}
+
+
+
+module.exports = { cadastrarUsuario, login};
